75c4ac6d04654baac719df4766d4e4f0f5e510d7
emamulk63-design <emamulk63@gmail.com>
2026-06-15 14:30:37 +0600

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,18 +1,30 @@
+const CACHE_NAME = 'joya-tv-v2';
+const urlsToCache = [
+  '/Joyavaii2026/',
+  '/Joyavaii2026/index.html',
+  '/Joyavaii2026/manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('joya-tv-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
